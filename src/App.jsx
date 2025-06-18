c360b86811c94bebf0fff5390d89fff4f53845d8
Victor Rojas <victoralonsorojassolorzano@gmail.com>
2025-06-17 19:50:23 -0500

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,21 @@ function App() {
   const [error, setError] = useState(null);
   const [language, setLanguage] = useState(true)
 
-  const handleClick=()=>{
-    setLanguage(!language);
-    setSearchTerm(searchResult);
-    setSearchResult(searchTerm);
+  const handleClick=(isSpanish)=>{
+    // Solo cambiar si el idioma es diferente al actual
+    if (language !== isSpanish) {
+      setLanguage(isSpanish);
+      setSearchTerm(searchResult.charAt(0).toUpperCase() + searchResult.slice(1).toLowerCase());
+      setSearchResult(searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1).toLowerCase());
+    }
   }
   return (
     <>
     <header className='headerContainer'>
-    <h1>Vocabulario</h1>
+    <h1 className='header__h1'>Vocabulario</h1>
     <div className="buttonContainer">
-    <a onClick={handleClick}> Español </a>
-    <a onClick={handleClick}> Русский </a>
+    <a onClick={() => handleClick(true)}> Español </a>
+    <a onClick={() => handleClick(false)}> Русский </a>
     </div>
     </header>
     <div className="vocabularioContainer">
